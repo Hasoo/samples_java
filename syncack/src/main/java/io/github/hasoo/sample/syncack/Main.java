f6796f206cbf9bb39737d0e065c08b2166ec2a77
Hasoo Kim <vaxzeem@i-heart.co.kr>
2021-03-15 12:46:28 +0900

    using SynchronousQueue

--- syncack/src/main/java/io/github/hasoo/sample/syncack/Main.java
+++ syncack/src/main/java/io/github/hasoo/sample/syncack/Main.java
@@ -1,5 +1,28 @@
 package io.github.hasoo.sample.syncack;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class Main {
 
+  public static void main(String[] args) throws ExecutionException, InterruptedException {
+    SyncAck<String> syncAck = new SyncAck<>();
+
+    ExecutorService service = Executors.newCachedThreadPool();
+
+    Future<Void> result = service.submit(() -> {
+      log.debug("start");
+      Thread.sleep(3000);
+      syncAck.save("Hasoo");
+      return null;
+    });
+
+    log.debug(syncAck.receive(10, TimeUnit.SECONDS));
+    service.shutdown();
+  }
 }
--- syncack/src/main/java/io/github/hasoo/sample/syncack/SyncAck.java
+++ syncack/src/main/java/io/github/hasoo/sample/syncack/SyncAck.java
@@ -1,2 +1,17 @@
-package io.github.hasoo.sample.syncack;public class SyncAck {
+package io.github.hasoo.sample.syncack;
+
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
+
+public class SyncAck<T> {
+
+  private final SynchronousQueue<T> queue = new SynchronousQueue<>();
+
+  public T receive(long timeout, TimeUnit timeUnit) throws InterruptedException {
+    return queue.poll(timeout, timeUnit);
+  }
+
+  public void save(T t) throws InterruptedException {
+    queue.put(t);
+  }
 }
